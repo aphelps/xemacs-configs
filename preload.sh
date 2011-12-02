8752251d164f95624f8fbbb1abbb345a49c28583
Adam Phelps <amp@opendns.com>
2011-12-01 22:46:42 -0800

    * Updated preload

--- preload.sh
+++ preload.sh
@@ -1,3 +1,3 @@
 # Create a preload.el emacs script based on a list of files, ignoring lines
 # starting with #
-awk '! /^#/ { print "(find-file \""$1"\")" }' ~/.xemacs/preload > ~/.xemacs/preload.el
+awk '! /^#/ { if (length($1) > 0) print "(find-file \""$1"\")" }' ~/.xemacs/preload > ~/.xemacs/preload.el
