9c0bfd4877f1f94d7549b3b80172b041ee7585af
Adam Phelps <amp@cs.stanford.edu>
2012-03-27 20:52:44 -0700

    * Adam's macbook

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 
 ;; Enable cscope
 ;; Uncomment this next line for MacOS
-;;(load-file "/opt/local/share/emacs/site-lisp/xcscope.el")
+(load-file "/opt/local/share/emacs/site-lisp/xcscope.el")
 (require 'xcscope)
 
 (load "~/.xemacs/my-c-style")
--- preload.el
+++
@@ -1,3 +0,0 @@
-(find-file "/home/amp/.xemacs/preload")
-(find-file "/home/amp/src/opendnscache.git/opendnscache/lib-opendnscache/dnscache.c")
-(find-file "/home/amp/src/opendnscache.git/opendnscache/exe-opendnscache/main.c")
